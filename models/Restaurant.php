3e159c6aba240c281e7fec5f778a678b90dea729
Kevin <kevin.firminmsl@gmail.com>
2017-01-19 17:37:18 -0600

    Created Restaurant Controller, Model and related Views for CHOWSER

---
+++ models/Restaurant.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "Restaurant".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $street1
+ * @property string $street2
+ * @property string $city
+ * @property string $state
+ * @property integer $zip
+ */
+class Restaurant extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'Restaurant';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'street1', 'city', 'zip'], 'required'],
+            [['zip'], 'integer'],
+            [['name', 'street1', 'street2', 'city'], 'string', 'max' => 100],
+            [['state'], 'string', 'max' => 2],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'street1' => 'Street1',
+            'street2' => 'Street2',
+            'city' => 'City',
+            'state' => 'State',
+            'zip' => 'Zip',
+        ];
+    }
+}
---
+++ views/restaurant/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Restaurants';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="restaurant-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Restaurant', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'street1',
+            'street2',
+            'city',
+            // 'state',
+            // 'zip',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>

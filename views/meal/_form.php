cb2d9b38fe4840d5224c3c11b65caa2138e91509
Steven <srw1267@gmail.com>
2017-01-24 18:58:06 -0600

    Add views for location-type and meal-type and all associated files.

--- views/meal/_form.php
+++ views/meal/_form.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use app\models\Restaurant;
+use app\models\MealType;
 use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
@@ -24,7 +25,9 @@ use yii\helpers\ArrayHelper;
             ArrayHelper::map(Restaurant::find()->asArray()->all(), 'id', 'name')
     ); ?>
 
-    <?= $form->field($model, 'mealTypeID')->textInput() ?>
+    <?= $form->field($model, 'mealTypeID')->dropDownList(
+        ArrayHelper::map(MealType::find()->asArray()->all(), 'id', 'mealTypeName')
+    ); ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>

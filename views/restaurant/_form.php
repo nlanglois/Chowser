7bbe3d8e34291c70a980c478a1311d76db6185b9
Nikolai Langlois <nlangloi10@umphysicians.umn.edu>
2017-02-02 17:48:10 -0700

    Fleshed out Restaurant model validation and cleaned up _form view for better UI/UX

--- views/restaurant/_form.php
+++ views/restaurant/_form.php
@@ -14,7 +14,7 @@ use app\models\LocationType;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true])->textInput(['placeholder' => "Name of restaurant"]) ?>
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true])->textInput(['placeholder' => "Name of restaurant"])->hint('Please enter the restaurant\'s name') ?>
 
     <?= $form->field($model, 'street1')->textInput(['maxlength' => true])->textInput(['placeholder' => "First line of street address"]) ?>
 
@@ -27,7 +27,7 @@ use app\models\LocationType;
     <?= $form->field($model, 'state')->dropdownList(
         \Yii::$app->params['us_states'],
         ['prompt'=>'Select US state']
-    );
+    )->hint('Please choose the state that this restaurant is located in');
     ?>
 
     <?= $form->field($model, 'zip')->textInput()->textInput(['placeholder' => "Restaurant zip code"]) ?>

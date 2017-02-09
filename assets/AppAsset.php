8663f1a0e28629a35a66a2cbd426295a9e508a4b
Steven <srw1267@gmail.com>
2017-02-09 17:18:37 -0600

    Added stylesheets to css folder of web, and added the link in the AppAsset

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -19,6 +19,8 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/chowser.css',
+        'css/styles/css',
     ];
     public $js = [
     ];

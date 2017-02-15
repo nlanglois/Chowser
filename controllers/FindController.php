9fd5f141fe474d51e6a94e03056c4c2d4ac34b56
Steven <srw1267@gmail.com>
2017-02-14 18:55:01 -0600

    Attempt at front end of informtion from Restaurant index view.

--- controllers/FindController.php
+++ controllers/FindController.php
@@ -2,6 +2,11 @@
 
 namespace app\controllers;
 
+use app\models\LocationType;
+use yii\data\ActiveDataProvider;
+use app\models\Restaurant;
+use app\models\Meal;
+
 class FindController extends \yii\web\Controller
 {
     public function actionBylocationproximity()
@@ -11,17 +16,29 @@ class FindController extends \yii\web\Controller
 
     public function actionBylocationtype()
     {
+        $dataProvider = new ActiveDataProvider([
+            'query' => LocationType::find(),
+        ]);
         return $this->render('bylocationtype');
     }
 
     public function actionBymeal()
     {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Meal::find(),
+        ]);
         return $this->render('bymeal');
     }
 
     public function actionByrestaurant()
     {
-        return $this->render('byrestaurant');
+        $dataProvider = new ActiveDataProvider([
+            'query' => Restaurant::find(),
+        ]);
+
+        return $this->render('byrestaurant', [
+            'dataProvider' => $dataProvider,
+        ]);
     }
 
 }
--- views/find/byrestaurant.php
+++ views/find/byrestaurant.php
@@ -1,9 +1,27 @@
 <?php
 /* @var $this yii\web\View */
+
+use yii\helpers\Html;
+use app\models\Restaurant;
+
 ?>
-<h1>find/byrestaurant</h1>
+<h1>Find by Restaurant</h1>
+
+<h3>Where would you like to eat?</h3>
 
 <p>
     You may change the content of this page by modifying
     the file <code><?= __FILE__; ?></code>.
 </p>
+
+<div class="all-restaurants">
+<?php
+    foreach ($dataProvider->getModels() as $model)
+    {
+        print Html::tag('div', Html::img(Yii::getAlias('@web') . '/' . $model->getUploadedFilePath(), ['width' => '100', 'alt' => 'Primary image for ' . Html::encode($model->name)]));
+        print Html::tag('p', $model->name);
+    }
+
+?>
+
+</div>

8bac54520bbce5376c29b9e6b82879270b129150
Steven <srw1267@gmail.com>
2017-02-16 10:30:49 -0600

    Got rid ofthe div around each "a" element. Added some margin, padding and width to each "a" element and also added media queries for responsiveness.

--- views/find/byrestaurant.php
+++ views/find/byrestaurant.php
@@ -9,26 +9,18 @@ use yii\helpers\Html;
 
 <h3>Where would you like to eat?</h3>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
-
 <div class="all-restaurants">
     <?php
         foreach ($dataProvider->getModels() as $model)
         {
-            print Html::tag(
-                    'div class="col-md-4 col-sm-12 col-xs-12"',
-                        Html::a(
-                            Html::img(
-                                Yii::getAlias('@web') . '/' . $model->getUploadedFilePath(),
-                                [
-                                    'width' => '100',
-                                    'alt' => 'Primary image for ' . Html::encode($model->name)
-                                ]
-                            ) . Html::tag('p', $model->name)
-                        )
+            print Html::a(
+                Html::img(
+                    Yii::getAlias('@web') . '/' . $model->getUploadedFilePath(),
+                    [
+                        'width' => '100',
+                        'alt' => 'Primary image for ' . Html::encode($model->name)
+                    ]
+                ) . Html::tag('p', $model->name)
             );
 
         }

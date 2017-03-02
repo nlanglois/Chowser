19f85033c3482cc851ce41e5c84908a39001ae67
Steven <srw1267@gmail.com>
2017-03-02 11:53:27 -0600

    Add more meals to our list of meals with images.

--- controllers/RestaurantReviewController.php
+++ controllers/RestaurantReviewController.php
@@ -2,9 +2,12 @@
 
 namespace app\controllers;
 
+use app\models\Restaurant;
 use Yii;
 use yii\web\Controller;
 use app\models\RestaurantReview;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
 
 class RestaurantReviewController extends Controller
 {
@@ -44,9 +47,26 @@ class RestaurantReviewController extends Controller
 
 
 
-    public function actionShowAll()
+    public function actionShowAll($id)
     {
-        return $this->render('show-all');
+        $queryRestaurantReviews = new ActiveDataProvider([
+            'query' => RestaurantReview::find()->where(['restaurantId' => $id]),
+        ]);
+        return $this->render('show-all', [
+            'model' => $this->findModel($id),
+            'restaurantReview' => $queryRestaurantReviews,
+        ]);
+
+
+    }
+
+    protected function findModel($id)
+    {
+        if (($model = RestaurantReview::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
     }
 
 }

493a74967fede0947d88b9a0fd1c0b0797594c72
Kevin <kevin.firminmsl@gmail.com>
2017-03-07 17:02:31 -0600

    Location proximity marker images and names + Show status of restaurant

--- views/find/bylocationproximity.php
+++ views/find/bylocationproximity.php
@@ -27,12 +27,12 @@ $this->title = 'Find by Location proximity';
 <?php
 
     $restaurantLocations = Restaurant::find()
-        ->select('name, coordinates')
-        ->where(['status' => 'Y'])
+        ->select('name, coordinates, photo')
+        ->Where(['status' => 'Y'])
         ->asArray()
         ->all();
 
-    //echo VarDumper::dumpAsString($restaurantLocations, 10, true);
+   // echo VarDumper::dumpAsString($restaurantLocations, 10, true);
 ?>
 
 
@@ -71,12 +71,13 @@ $this->title = 'Find by Location proximity';
 
         marker = new google.maps.Marker({
             position: new google.maps.LatLng(lat, lon),
-            map: map
+            map: map,
+            animation: google.maps.Animation.DROP
         });
 
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
-                infowindow.setContent(locations[i].name);
+                infowindow.setContent("<img style='width: 200px' src='/web/uploads/restaurant/" + locations[i].photo + "'/> </br>" + locations[i].name);
                 infowindow.open(map, marker);
             }
         })(marker, i));

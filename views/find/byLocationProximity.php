ed0c78eb623c8259d0489d0a99089496705b34f9
Steven <srw1267@gmail.com>
2017-03-19 09:46:27 -0500

    Added comments to the LocationProximity page for the Restaurant Hours.

--- views/find/byLocationProximity.php
+++ views/find/byLocationProximity.php
@@ -44,6 +44,7 @@ $this->title = 'Find by Location proximity';
 
         $restaurantLocations[$key]['hours'] = $restaurantHours;
     }
+    // SELECT dayOfWeek, open, close FROM RestaurantHours WHERE the array restId = an array for 'id'
 
 //    echo VarDumper::dumpAsString($restaurantLocations, 10, true);
 ?>

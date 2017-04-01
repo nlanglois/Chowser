9b67c6c96f4e5a49730a76060284c55daf46aa02
Nikolai Langlois <nlangloi10@umphysicians.umn.edu>
2017-04-01 18:41:39 -0500

    Visual tweaks to users views in /admin; page titles, headers, etc.

--- views/users/index.php
+++ views/users/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Users', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create new user', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
--- views/users/update.php
+++ views/users/update.php
@@ -5,9 +5,9 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Users */
 
-$this->title = 'Update Users: ' . $model->id;
+$this->title = 'Update this user: ' . $model->fName . " " . $model->lName;
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->fName . " " . $model->lName, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="users-update">
